cd80f2a3925025ac9155c29d58bc99e73b9fa3cb
Charles Arboleda <charlesarboleda_@hotmail.com>
2022-11-22 19:48:52 +0000

    Small function change

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
 const loginRoutes = require('./routes/login')
+const registerRoutes = require('./routes/register')
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -41,6 +42,7 @@ app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 // Note: mount other resources here, using the same pattern above
 app.use('/login', loginRoutes);
+app.use('/register', registerRoutes)
 
 // Home page
 // Warning: avoid creating more routes in this file!

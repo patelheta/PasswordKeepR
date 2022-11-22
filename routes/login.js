7af977a009f5bc7c364c43d4a03fb35a99ebc3ee
Charles Arboleda <charlesarboleda_@hotmail.com>
2022-11-22 02:14:03 +0000

    Add login routes/ejs/scss

---
+++ routes/login.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router  = express.Router();
+const cookieSession = require('cookie-session');
+
+router.use(cookieSession({
+  name: 'session',
+  keys: ['CHARLES'],
+  maxAge: 24 * 60 * 60 * 1000
+}));
+
+router.get('/', (req, res) => {
+  res.render('login');
+});
+
+router.post('/', (req, res) => {
+  res.redirect('/login/:id');
+});
+
+router.get('/:id', (req, res) => {
+  req.session.user_id = req.params.id;
+  res.redirect('/');
+});
+
+router.post('/register', (req, res) => {
+  res.redirect('/register');
+})
+
+module.exports = router;

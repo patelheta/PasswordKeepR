113095a0601125f16bbd00454fd33bfa921b64ad
Darren Lin <darrenlin815@hotmail.com>
2022-11-22 18:33:48 -0800

    Refactored helper functions from /db/queries into helper functions file

--- helpers.js
+++ helpers.js
@@ -1,8 +1,15 @@
 const argon2 = require('argon2');
 const { Pool } = require('pg');
-const dbParams = require("./lib/db.js");
+const dbParams = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+};
 const db = new Pool(dbParams);
 db.connect();
+
 
 const addNewUser = async function (user) {
   try {
@@ -23,6 +30,7 @@ const addNewUser = async function (user) {
 };
 exports.addNewUser = addNewUser;
 
+
 const getUserByEmail = async function (email) {
   try {
     const user = await db.query(`SELECT * FROM users WHERE email = $1`, [
@@ -37,3 +45,48 @@ const getUserByEmail = async function (email) {
   }
 };
 exports.getUserByEmail = getUserByEmail;
+
+
+const getAllPasswords = () => {
+  let queryString = `SELECT * FROM user_websites;`;
+  return db.query(queryString)
+    .then(data => {
+      console.log(data.rows);
+      return data.rows;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = { getAllPasswords };
+
+
+const getOrganizations = () => {
+  return db.query('SELECT * FROM organizations;')
+    .then(data => {
+      console.log(data.rows);
+      return data.rows;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = { getOrganizations };
+
+
+const db = require('../connection');
+
+const getcategories = () => {
+  return db.query('SELECT * FROM categories;')
+    .then(data => {
+      console.log(data.rows);
+      return data.rows;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = { getcategories };
